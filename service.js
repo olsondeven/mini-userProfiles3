e6f113d8e8d85771da3e7dc7d1d540c2c241eacb
Deven Olson <olsondeven@gmail.com>
2016-10-27 12:10:28 -0600

    finished

--- service.js
+++ service.js
@@ -1,10 +1,24 @@
-angular.module('userProfiles').service('mainService', function($http) {
+angular.module('userProfiles').service('mainService', function($http, $q) {
 
   this.getUsers = function() {
-    return $http({
+    var dererred = $q.defer();
+    $http({
         method: 'GET',
         url: 'http://reqres.in/api/users?page=1'
+    }).then(function(response){
+
+      var people = response.data.data;
+
+      for (var i = 0; i < array.length; i++) {
+        var element = array[i];
+        element.first_name = 'Ralph';
+      }
+
+      defer.resolve(people);
+
     })
+
+    return defer.promise;
   }
 
-});
+});//closing bracket
